2c35465a4338518e25886a6cfe0cb47a8a4c19ba
checkaayush <checkaayush@gmail.com>
2022-11-23 13:57:40 +0530

    Add dummy func and test

--- log/alerts_test.go
+++ log/alerts_test.go
@@ -15,3 +15,12 @@ func TestAlert(t *testing.T) {
 	assert.Equal(a.Priority(), AlertP0)
 	assert.Equal(a.String(), "P0::testSvc::type1")
 }
+
+func TestAdd(t *testing.T) {
+
+	assert := require.New(t)
+
+	assert.Equal(add(1, 2), 3)
+	assert.Equal(add(-1, 2), 1)
+	assert.Equal(add(0, 2), 2)
+}
